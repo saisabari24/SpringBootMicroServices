d44797eed2795b9685722d7eb7c61e836c13f658
saisabari24 <sai77sab@gmail.com>
2023-12-14 02:06:29 +0530

    Created Eureka Discovery Server and added Client dependencies in pom.xml of each server

--- order-service/src/main/java/com/Microservices/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/Microservices/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -39,8 +39,8 @@ public class OrderService {
                 .toList();
 
         //Call inventory Service and place order if products is in stock
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                 uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
